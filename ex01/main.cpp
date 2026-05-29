ca47b8875e8b3932e302055ee35ce935243a480d
Davit Grigoryan <dgrigor2@c2r11s6.42yerevan.am>
2026-05-29 16:52:17 +0400

    thinking about making hitpoint and energypoint checks separate methods

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,8 +1,57 @@
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include <iostream>
+
+// int main()
+// {
+// 	ScavTrap tmp("banan");
+
+// 	for (int i = 0; i < 3; i++)
+// 		tmp.takeDamage(60);
+// }
 
 int main()
 {
-	ClapTrap tmp("Valod");
+	std::cout << "=== Creating ClapTrap ===" << std::endl;
+	ClapTrap clap("CL4P-TP");
+
+	std::cout << std::endl;
+
+	std::cout << "=== Creating ScavTrap ===" << std::endl;
+	ScavTrap scav("SC4V-TP");
+
+	std::cout << std::endl;
+
+	std::cout << "=== ClapTrap tests ===" << std::endl;
+	clap.attack("bandit");
+	clap.takeDamage(5);
+	clap.beRepaired(3);
+
+	std::cout << std::endl;
+
+	std::cout << "=== ScavTrap tests ===" << std::endl;
+	scav.attack("psycho");
+	scav.takeDamage(30);
+	scav.beRepaired(10);
+	scav.guardGate();
+
+	std::cout << std::endl;
+
+	std::cout << "=== Energy depletion test ===" << std::endl;
+	for (int i = 0; i < 55; i++)
+	{
+		scav.attack("target");
+	}
+
+	std::cout << std::endl;
+
+	std::cout << "=== HP depletion test ===" << std::endl;
+	scav.takeDamage(200);
+	scav.attack("another target");
+	scav.beRepaired(10);
+
+	std::cout << std::endl;
+	std::cout << "=== End of program ===" << std::endl;
 
 	return (0);
 }
